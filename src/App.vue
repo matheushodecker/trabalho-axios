57b08435850ea02544e81556bc3aa57b36b28f62
Marco André Mendes <marcoandre@gmail.com>
2023-09-28 14:52:55 -0300

    router view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import api from './plugins/axios'
-
-const moviesGenres = ref([])
-const TVGenres = ref([])
-
-onMounted(async () => {
-  let response = await api.get('genre/movie/list?language=pt-BR')
-  moviesGenres.value = response.data.genres
-  response =  await api.get('genre/tv/list?language=pt-BR')
-  TVGenres.value = response.data.genres
-})
 </script>
 
 <template>
-  <h1>Gêneros de filmes</h1>
-  <ul>
-    <li v-for="genre in moviesGenres" :key="genre.id">
-      {{ genre.name }} 
-    </li>
-  </ul>
-  <hr />
-  <h1>Gêneros de séries</h1>
-  <ul>
-    <li v-for="genre in TVGenres" :key="genre.id">
-      {{ genre.name }}
-    </li>
-  </ul>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 1rem;
+  column-gap: 2rem;
+}
+</style>
