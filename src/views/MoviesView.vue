5a9ea3ade16215fc1eb66160f65b0ec7d0bf7939
Marco André Mendes <marcoandre@gmail.com>
2023-09-28 15:03:25 -0300

    Ajustes estilos CSS

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -11,12 +11,34 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <h1>Gêneros de filmes</h1>
-    <ul>
-      <li v-for="genre in genres" :key="genre.id">
-        {{ genre.name }}
+    <h1>Filmes</h1>
+    <ul class="genre-list">
+      <li v-for="genre in genres" :key="genre.id" class="genre-item">
+          {{ genre.name }}
       </li>
     </ul>
-  </div>
-</template>
+  </template>
+
+<style scoped>
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
+
+.genre-item {
+  background-color: #387250;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
+}
+</style>
