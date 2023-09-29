baa3153d75290211e8deae8a635ab06caf764e9b
Marco André Mendes <marcoandre@gmail.com>
2023-09-29 11:38:40 -0300

    formatação de data

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -8,11 +8,7 @@ const genres = ref([])
 const movies = ref([]);
 
 const getGenreName = (id) => genres.value.find((genre) => genre.id === id).name
-
-onMounted(async () => {
-  const response = await api.get('genre/movie/list?language=pt-BR')
-  genres.value = response.data.genres
-})
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
 
 const listMovies = async (genreId) => {
   isLoading.value = true
@@ -25,6 +21,11 @@ const listMovies = async (genreId) => {
   movies.value = response.data.results
   isLoading.value = false
 }
+
+onMounted(async () => {
+  const response = await api.get('genre/movie/list?language=pt-BR')
+  genres.value = response.data.genres
+})
 </script>
 
 <template>
@@ -43,7 +44,7 @@ const listMovies = async (genreId) => {
       <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
       <div class="movie-details">
         <p class="movie-title">{{ movie.title }}</p>
-        <p class="movie-release-date">{{ movie.release_date }}</p>
+        <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
         <p class="movie-genres">
           <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
             {{ getGenreName(genre_id) }}
