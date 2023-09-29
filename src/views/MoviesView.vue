f079822a35883b5e14601d21184c5018fbfb9dd6
Marco André Mendes <marcoandre@gmail.com>
2023-09-29 09:01:19 -0300

    listando filmes por gênero

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -8,16 +8,41 @@ onMounted(async () => {
   const response = await api.get('genre/movie/list?language=pt-BR')
   genres.value = response.data.genres
 })
+
+const movies = ref([]);
+
+const listMovies = async (genreId) => {
+  const response = await api.get('discover/movie', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  });
+  movies.value = response.data.results
+};
 </script>
 
 <template>
-    <h1>Filmes</h1>
-    <ul class="genre-list">
-      <li v-for="genre in genres" :key="genre.id" class="genre-item">
-          {{ genre.name }}
-      </li>
-    </ul>
-  </template>
+  <h1>Filmes</h1>
+  <ul class="genre-list">
+    <li v-for="genre in genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item">
+      {{ genre.name }}
+    </li>
+  </ul>
+
+  <div class="movie-list">
+  <div v-for="movie in movies" :key="movie.id" class="movie-card">
+    
+    <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+    <div class="movie-details">
+      <p class="movie-title">{{ movie.title }}</p>
+      <p class="movie-release-date">{{ movie.release_date }}</p>
+      <p class="movie-genres">{{ movie.genre_ids }}</p>
+    </div>
+    
+  </div>
+</div>
+</template>
 
 <style scoped>
 .genre-list {
@@ -26,8 +51,9 @@ onMounted(async () => {
   flex-wrap: wrap;
   gap: 2rem;
   list-style: none;
-  padding: 0;
+  margin-bottom: 2rem;
 }
+
 
 .genre-item {
   background-color: #387250;
@@ -40,5 +66,37 @@ onMounted(async () => {
   cursor: pointer;
   background-color: #4e9e5f;
   box-shadow: 0 0 0.5rem #387250;
+}
+
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.movie-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-details {
+  padding: 0 0.5rem;
+}
+
+.movie-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
 }
 </style>
