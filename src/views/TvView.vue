b0ce5e6db2e04916d3c24e45441d09c51e3298a2
Marco André Mendes <marcoandre@gmail.com>
2023-11-21 14:55:58 -0300

    Refactor TVView component to display TV shows based on genre

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -4,6 +4,18 @@ import api from '@/plugins/axios'
 
 const genres = ref([])
 
+const tv_shows = ref([]);
+
+const listTvShows = async (genreId) => {
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  });
+  tv_shows.value = response.data.results
+};
+
 onMounted(async () => {
   const response = await api.get('genre/tv/list?language=pt-BR')
   genres.value = response.data.genres
@@ -11,37 +23,82 @@ onMounted(async () => {
 </script>
 
 <template>
-    <h1>Programas de TV</h1>
-    <ul class="genre-list">
-      <li v-for="genre in genres" :key="genre.id" class="genre-item">
-          {{ genre.name }}
-      </li>
-    </ul>
-  </template>
-  
-  <style scoped>
-  .genre-list {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2rem;
-    list-style: none;
-    padding: 0;
-  }
-  
-  .genre-item {
-    background-color: #5d6424;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    align-self: center;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-  }
-  
-  .genre-item:hover {
-    cursor: pointer;
-    background-color: #7d8a2e;
-    box-shadow: 0 0 0.5rem #5d6424;
-  }
-  </style>
+  <h1>Programas de TV</h1>
+  <ul class="genre-list">
+    <li v-for="genre in genres" :key="genre.id" @click="listTvShows(genre.id)" class="genre-item">
+      {{ genre.name }}
+    </li>
+  </ul>
+
+  <div class="tvShow-list">
+    <div v-for="tvShow in tv_shows" :key="tvShow.id" class="tvShow-card">
+
+      <img :src="`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`" :alt="tvShow.name" />
+      <div class="tvShow-details">
+        <p class="tvShow-title">{{ tvShow.name }}</p>
+        <p class="tvShow-release-date">{{ tvShow.first_air_date }}</p>
+        <p class="tvShow-genres">{{ tvShow.genre_ids }}</p>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
+
+.genre-item {
+  background-color: #5d6424;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  align-self: center;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #7d8a2e;
+  box-shadow: 0 0 0.5rem #5d6424;
+}
+
+.tvShow-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tvShow-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tvShow-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tvShow-details {
+  padding: 0 0.5rem;
+}
+
+.tvShow-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+</style>
